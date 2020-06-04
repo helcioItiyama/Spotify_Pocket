b5fca4f9632617a7fcaed6e7dbe46d754820d27d
Helcio Itiyama <helcio.itiyama@gmail.com>
2020-06-04 12:35:05 -0300

    readme

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export const config = {
   spotify: {
     authorizationURL: 'https://accounts.spotify.com/authorize',
-    clientId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    clientId: //'use your clientId here'//,
     redirectUrl: `${window.location.origin}/authorize`,
     webAPI: 'https://api.spotify.com/v1',
     scopes: [
